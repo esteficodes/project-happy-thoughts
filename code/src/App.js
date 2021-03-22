57b000e084cb979b98de8f6baa29a1e63390f6a8
esteficodes <estefaniaquevedolusby@gmail.com>
2021-03-22 15:50:42 +0100

    created components for the form and the messages and imported them

--- code/src/App.js
+++ code/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Form } from 'Components/Form';
+import { MessagesCard } from 'Components/MessagesCard';
 
 export const App = () => {
   return (
