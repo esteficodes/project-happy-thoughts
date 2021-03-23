2b20da25a71f38b62b1df07ce505f5a631b618ad
esteficodes <estefaniaquevedolusby@gmail.com>
2021-03-23 11:01:13 +0100

    created variables for messages updating

--- code/src/App.js
+++ code/src/App.js
@@ -5,6 +5,7 @@ import { API_URL } from './reusable/urls';
 
 export const App = () => {
   const [messageList, setMessageList] = useState([])
+  const [messageNew, setMessageNew] = useState('');
 //useEffect should always be imported here right after the function declaration.
   useEffect(() => {
     fetchMessageList();
@@ -20,9 +21,24 @@ export const App = () => {
   }
 
   fetchMessageList();
-//Here gos the jsx so there should be added everything that we want to show on the app
-  return (
+//Here goes the jsx so there should be added everything that we want to show on the app. 
+//Below I'm adding a form that will show the messages. this form can be a component on its own.
+const onMessageNewChange = (event) => {
+  setMessageNew()
+}
+
+return (
     <div>
+      <form>
+        <label htmlFor="newMessage">Write a new message!</label>
+        <input 
+          id="newMessage"
+          type="text"
+          value={messageNew}
+          onChange
+          />
+      </form>
+
       {messageList.map(message => (
         <div key={message._id}>
           <h4>{message.text}</h4>
