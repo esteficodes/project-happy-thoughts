907bf25bb0bf684d274d4b0997a3ee94d42f3e89
esteficodes <estefaniaquevedolusby@gmail.com>
2021-03-23 10:44:57 +0100

    added jsx for messages and date, installed moment package

--- code/src/App.js
+++ code/src/App.js
@@ -1,26 +1,34 @@
-import React, { useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { API_URL } from './reusable/urls';
 
 export const App = () => {
-
+  const [messageList, setMessageList] = useState([])
+//useEffect should always be imported here right after the function declaration.
   useEffect(() => {
-    
-  })
+    fetchMessageList();
 
+  }, []);
+//fetching for the messages array
   const fetchMessageList = () => {
     fetch(API_URL)
     .then(res => res.json())
-    .then(messages => console.log(messages))
+    .then(messages => setMessageList(messages))
     .catch(err => console.err(err));
     
   }
 
   fetchMessageList();
-
+//Here there should be added everything that we want to show on the app
   return (
     <div>
-      
+      {messageList.map(message => (
+        <div key={message._id}>
+          <h4>{message.text}</h4>
+          <p>{new Date(message.createdAt).toDateString()}</p>
+        </div>
+
+     ))}
     </div>
   )
 }
